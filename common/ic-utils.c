54bf8e87dd0586df6c78660b31f442ca7765d133
sung.goo.kim <sung.goo.kim@samsung.com>
2016-05-23 16:01:08 +0900

    Rename from OIC(Open Interconnect Consortium) to OCF(Open Connectivity Foundation)
    Change-Id: I883cb5af3b182f3a6e0c0510d4db25af86297c67

--- common/ic-utils.c
+++ common/ic-utils.c
@@ -33,9 +33,13 @@
 #ifdef TZ_VER_3
 static int _ic_oic_feature = -1;
 static const char *IC_FEATURE_OIC = "http://tizen.org/feature/iot.oic";
+static const char *IC_FEATURE_OCF = "http://tizen.org/feature/iot.ocf";
+
 
 static int _ic_oic_security_feature = -1;
 static const char *IC_FEATURE_OIC_SECURITY = "http://tizen.org/feature/iot.oic.security";
+static const char *IC_FEATURE_OCF_SECURITY = "http://tizen.org/feature/iot.ocf.security";
+
 #endif
 
 // TODO: Can't access in user side daemon
@@ -146,8 +150,14 @@ bool ic_utils_check_oic_feature()
 #ifdef TZ_VER_3
 	if (_ic_oic_feature < 0) {
 		bool feature_supported = false;
-		system_info_get_platform_bool(IC_FEATURE_OIC, &feature_supported);
+		system_info_get_platform_bool(IC_FEATURE_OCF, &feature_supported);
 		_ic_oic_feature = feature_supported ? 1 : 0;
+
+		// TODO: Remove this code when applied oic name changes
+		if (_ic_oic_feature == 0) {
+			system_info_get_platform_bool(IC_FEATURE_OIC, &feature_supported);
+			_ic_oic_feature = feature_supported ? 1 : 0;
+		}
 	}
 	return _ic_oic_feature;
 #else
@@ -160,8 +170,14 @@ bool ic_utils_check_oic_security_feature()
 #ifdef TZ_VER_3
 	if (_ic_oic_security_feature < 0) {
 		bool feature_supported = false;
-		system_info_get_platform_bool(IC_FEATURE_OIC_SECURITY, &feature_supported);
+		system_info_get_platform_bool(IC_FEATURE_OCF_SECURITY, &feature_supported);
 		_ic_oic_security_feature = feature_supported ? 1 : 0;
+
+		// TODO: Remove this code when applied oic name changes
+		if (_ic_oic_security_feature == 0) {
+			system_info_get_platform_bool(IC_FEATURE_OIC_SECURITY, &feature_supported);
+			_ic_oic_security_feature = feature_supported ? 1 : 0;
+		}
 	}
 	return _ic_oic_security_feature;
 #else
